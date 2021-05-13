b6d47d88fcbdb9c81e0781f1b626c93c95366eb2
moose <mr.rammo@gmail.com>
2021-05-13 19:53:27 -0400

    Create Auth.js

---
+++ Auth.js
@@ -0,0 +1,57 @@
+/**
+ * identity
+ * Copyright(c) 2021 Mostapha Rammo
+ * MIT Licensed
+ */
+
+/**
+ * Module Dependencies
+ * @private
+ */
+const bcrypt = require('bcryptjs');
+
+/**
+ * Auth Prototype
+ */
+
+const Auth = {};
+exports = Auth;
+module.exports = Auth;
+
+/**
+ * Hash a password
+ * @param {String} plainText plain text password to be hashed
+ * @returns {Promise} Promise
+ */
+Auth.hashPassword = function hashPassword(plainText) {
+  if (!plainText || typeof plainText !== 'string') {
+    throw new TypeError('expected a string to hash');
+  }
+
+  return bcrypt
+    .hash(plainText, 10)
+    .catch((error) => {
+      throw error;
+    });
+};
+
+/**
+ * Compare text to a hash
+ * @param {String} plainText text to be compared
+ * @param {String} hash hash that text will be compared to
+ * @returns {Promise} Promise
+ */
+Auth.comparePassword = function comparePassword(plainText, hash) {
+  if (!plainText || typeof plainText !== 'string') {
+    throw new TypeError('expected a plain text string to compare');
+  }
+  if (!hash || typeof hash !== 'string') {
+    throw new TypeError('expected hash to compare to');
+  }
+
+  return bcrypt
+    .compare(plainText, hash)
+    .catch((error) => {
+      throw error;
+    });
+};
